213dd05bc67ded0468dac2d7b124843ecc705d97
Jai <jai@doselect.com>
2016-03-31 15:15:48 +0530

    Added bare minimum terminal
    Signed-off-by: Jai <jai@doselect.com>

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,21 @@
-angular.module('dws', ['ngMaterial'])
+angular.module('dws', ['vtortola.ng-terminal'])
 
-.controller('appCtrl', ['$scope', function ($scope) {
+  .config(['terminalConfigurationProvider', function (terminalConfigurationProvider) {
+    terminalConfigurationProvider.config('modern').outputDelay = 10
+    terminalConfigurationProvider.config('modern').allowTypingWriteDisplaying = false
+  }])
 
-}])
+  .controller('terminalCtrl', ['$scope', '$rootScope',
+    function ($scope, $rootScope) {
+      $rootScope.theme = 'modern'
+
+    setTimeout(function () {
+      $scope.$broadcast('terminal-output', {
+        output: true,
+        text: ['Welcome to DoSelect Web Services CLI',
+               'Usage: dws [options] <command> <subcommand> [parameters]'],
+        breakLine: true
+      })
+      $scope.$apply()
+    }, 100)
+  }])
